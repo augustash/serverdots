ea7d374616e3d02ac95f0eeaa4886939b8513696
Pete McWilliams <petemcw@gmail.com>
2011-12-16 11:41:05 -0600

    Created alias for "git" itself

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,4 @@
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
