567995fad10c124edb82d5d6fca858028fbc4967
Pete McWilliams <petemcw@gmail.com>
2011-12-21 21:53:52 -0600

    Added basic listing aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,13 +1,4 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors
-if $(gls &>/dev/null) then;
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -lh --color"
-  alias la="gls -A --color"
-else
-  alias ls="ls -F --color"
-  alias l="ls -lAh --color"
-  alias ll="ls -lh --color"
-  alias la="ls -A --color"
-fi
+alias ls="ls -F --color"
+alias l="ls -lAh --color"
+alias ll="ls -lh --color"
+alias la="ls -A --color"
