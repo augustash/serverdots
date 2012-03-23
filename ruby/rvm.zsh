64534770539fd00d52cde0012353380e04c8d0fa
Pete McWilliams <pmcwilliams@augustash.com>
2012-03-23 17:36:10 -0500

    Removed *.zsh files in favor of keeping them within the Oh-my-zsh repo

--- ruby/rvm.zsh
+++
@@ -1,2 +0,0 @@
-# This loads RVM into a shell session
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
--- system/env.zsh
+++
@@ -1,7 +0,0 @@
-export EDITOR='vim'
-export GIT_EDITOR='vim'
-export SVN_EDITOR='vim'
-
-export TZ="/usr/share/zoneinfo/UTC"
-
-export TERM="xterm-256color"
